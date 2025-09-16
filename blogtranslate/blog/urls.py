7b87e1c1645e3592b2c37f33c1b9ef8722194732
Pleng_Nuttida <66070059@kmitl.ac.th>
2025-09-17 00:15:42 +0700

    add key

--- blogtranslate/blog/urls.py
+++ blogtranslate/blog/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("translate/<int:blog_id>/", views.translate_blog, name="translate_blog"),
 ]
--- blogtranslate/blog/views.py
+++ blogtranslate/blog/views.py
@@ -1,24 +1,26 @@
+from django.shortcuts import render
 from django.http import JsonResponse
 from google.cloud import translate_v2 as translate
 from .models import Blog
 
-# Create client once
 translate_client = translate.Client()
 
+def home(request):
+    blogs = Blog.objects.all()
+    return render(request, "blog/home.html", {"blogs": blogs})
+
 def translate_blog(request, blog_id):
-    target_lang = request.GET.get("lang", "en")  # default: English
+    target_lang = "en"  # always translate to English
     try:
         blog = Blog.objects.get(pk=blog_id)
     except Blog.DoesNotExist:
         return JsonResponse({"error": "Blog not found"}, status=404)
 
-    # Translate fields
     translated_header = translate_client.translate(blog.header, target_language=target_lang)["translatedText"]
     translated_body = translate_client.translate(blog.body, target_language=target_lang)["translatedText"]
 
     return JsonResponse({
         "blog_id": blog.id,
-        "target_lang": target_lang,
         "header": translated_header,
         "body": translated_body,
     })
